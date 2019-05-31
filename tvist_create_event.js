d5afff3d3a09122d3c93e507f02c90426986cdab
Mikhail Nikitin <soundgot@gmail.com>
2019-05-31 12:19:20 +0500

    Update tvist_create_event.js

--- tvist_create_event.js
+++ tvist_create_event.js
@@ -1,3 +1,26 @@
+function addEvent3(person1, person2, person3, name, comment, sday, eday, start, end, month, year)
+{
+
+	var i = sday;
+	while (i <= eday) {
+		/// LOOP HERE
+		var day = i;
+		Ext.Ajax.request({
+			url: 'coord/event',
+			params: {
+				xaction: 'create',
+				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":'+comment+',"user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true},{"staff_id":57,"user_id":'+person3+',"tabel_apparature":true}]}}'
+			},
+			callback: function(opt, suc, resp){
+				if (suc){
+					console.log(resp);
+				}
+			}
+		});
+		/// LOOP HERE END
+		i++;
+	}
+};
 function addEvent2(person1, person2, name, comment, sday, eday, start, end, month, year)
 {
 
@@ -21,29 +44,7 @@ function addEvent2(person1, person2, name, comment, sday, eday, start, end, mont
 		i++;
 	}
 };
-function addEvent2_noText(person1, person2, name, sday, eday, start, end, month, year)
-{
 
-	var i = sday;
-	while (i <= eday) {
-		/// LOOP HERE
-		var day = i;
-		Ext.Ajax.request({
-			url: 'coord/event',
-			params: {
-				xaction: 'create',
-				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":" ","user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true}]}}'
-			},
-			callback: function(opt, suc, resp){
-				if (suc){
-					console.log(resp);
-				}
-			}
-		});
-		/// LOOP HERE END
-		i++;
-	}
-};
 //ДИЗАЙНЕРЫ
 var Mamaev = 960;
 var Nikitin = 239;
@@ -76,4 +77,5 @@ var Malyshkin = 926;
 var Boroday = 242;
 
 
-addEvent_noText(Nikitin, Mamaev, "тест", "30", "30", "10:00", "11:00", "05", "2019");
+addEvent2(Nikitin, Mamaev, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "30", "30", "10:00", "11:00", "05", "2019"); //два участника
+addEvent3(Nikitin, Mamaev, Boroday, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "30", "30", "10:00", "11:00", "05", "2019"); // три участника
