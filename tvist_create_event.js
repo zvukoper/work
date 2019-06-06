9b41fcf2a662b0e03df22a9e33d9feadcbcf04cc
Mikhail Nikitin <soundgot@gmail.com>
2019-06-06 14:29:31 +0500

    Update tvist_create_event.js

--- tvist_create_event.js
+++ tvist_create_event.js
@@ -9,7 +9,7 @@ function addEvent3(person1, person2, person3, name, comment, sday, eday, start, 
 			url: 'coord/event',
 			params: {
 				xaction: 'create',
-				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":'+comment+',"user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true},{"staff_id":57,"user_id":'+person3+',"tabel_apparature":true}]}}'
+				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":"'+comment+'","user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true},{"staff_id":57,"user_id":'+person3+',"tabel_apparature":true}]}}'
 			},
 			callback: function(opt, suc, resp){
 				if (suc){
@@ -32,7 +32,7 @@ function addEvent2(person1, person2, name, comment, sday, eday, start, end, mont
 			url: 'coord/event',
 			params: {
 				xaction: 'create',
-				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":'+comment+',"user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true}]}}'
+				data: '{"event":{"type_id":4,"dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":"'+comment+'","user_id":'+person1+', "staff":[{"staff_id":57,"user_id":'+person2+',"tabel_apparature":true}]}}'
 			},
 			callback: function(opt, suc, resp){
 				if (suc){
@@ -55,7 +55,7 @@ function addEvent1(person1, name, comment, sday, eday, start, end, month, year)
 			url: 'coord/event',
 			params: {
 				xaction: 'create',
-				data: '{"event":{"type_id":3, "dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":'+comment+',"creator":872, "staff":[{"staff_id":57,"user_id":'+person1+',"tabel_apparature":true}]}}'
+				data: '{"event":{"type_id":3, "dragResize":true,"stopdate":"'+year+'-'+month+'-'+day+'T'+end+':00","startdate":"'+year+'-'+month+'-'+day+'T'+start+':00","name":"'+name+'","comment":"'+comment+'","creator":872, "staff":[{"staff_id":57,"user_id":'+person1+',"tabel_apparature":true}]}}'
 			},
 			callback: function(opt, suc, resp){
 				if (suc){
@@ -108,7 +108,7 @@ var Grishakov = 755;
 var Scherbakova = 379;
 var Ganieva = 91;
 
-// одного человека сделать трудно, потом буду рабираться. Твист дебильный. Он обязательно создает событие на двоих. Потом у события появляется номер и наряд, потом надо выгрузить этот номер. Потом отредактировать список людей и сохранить. Это 4 функции еще. Их так просто не раскурить пока.
-// addEvent1(Mamaev, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "06", "06", "10:00", "11:00", "06", "2019"); //один участник
+
+addEvent2(Nikitin, Mamaev, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "31", "31", "10:00", "11:00", "07", "2019");
 addEvent2(Nikitin, Mamaev, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "30", "30", "10:00", "11:00", "05", "2019"); //два участника
 addEvent3(Nikitin, Mamaev, Boroday, "НАЗВАНИЕ", "ОПИСАНИЕ (пробел если не нужно)", "30", "30", "10:00", "11:00", "05", "2019"); // три участника
